522ddea0023acc98c6e427690592c30c34184a3b
Sidharth D <zurajanaikatsurada1222@gmail.com>
2021-09-12 20:03:57 +0530

    File Ready

--- fc4.c
+++ fc4.c
@@ -1,4 +1,4 @@
-//C program to print contents in reverse order of a file
+// Q: C program to print contents in reverse order of a file
 
 #include <stdio.h>
 
@@ -10,10 +10,10 @@ void main(){
     printf("Enter the no of characters to count from reverse:");
     scanf("%d",&no);
     printf("Newline counts as two characters.\n");
-    for (int i=3;i<no+3;i++){
+    for (int i=3;i<no+3;i++){  // Here i is initially given as 3 because the last three elements read by the pointer are EOF and newline character.
         fseek(F,-i,2);
         c=fgetc(F);
-        if (c=='\n'){
+        if (c=='\n'){  // In order to not count newline character, adding no is done
             no++;
         }
         printf("%c",c);
