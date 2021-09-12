ecebfff5c9e3b61642fc5a55f33ffddeed89ef23
Sidharth D <zurajanaikatsurada1222@gmail.com>
2021-09-12 20:06:50 +0530

    File Ready

--- fc5.c
+++ fc5.c
@@ -1,4 +1,4 @@
-//Write a program to compare two file.
+// Q: Write a program to compare two file.
 
 #include <stdio.h>
 
@@ -6,13 +6,13 @@ int main(){
     FILE *F1,*F2;
     F1 = fopen("text1.txt","r");
     F2 = fopen("text.txt","r");
-    char ch1=fgetc(F1),ch2=fgetc(F2);
-    do{
+    char ch1=fgetc(F1),ch2=fgetc(F2); // Initially character is called to check first condition because its do while loop
+    do{  
         if (ch1!=ch2){
             printf("The contents are different.");
             return 1;
         }
-    }while((ch1=fgetc(F1))!=EOF && (ch2=fgetc(F2))!=EOF);
+    }while((ch1=fgetc(F1))!=EOF && (ch2=fgetc(F2))!=EOF);  // Do while is used intead of while for simplicity
     printf("The contents are same for both files.");
     return 0;
 }
